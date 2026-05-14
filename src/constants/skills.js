da45908e2bc0124031346aa872773be7746d12c1
Saraalsobah <salsobah2006@gmail.com>
2026-05-14 18:29:01 +0300

    completed Skill Picker component

---
+++ src/constants/skills.js
@@ -0,0 +1,53 @@
+const skills = [
+  // Programming & Tech
+  'JavaScript',
+  'React',
+  'Node.js',
+  'Express',
+  'MongoDB',
+  'Python',
+  'Java',
+  'SQL',
+  'HTML',
+  'CSS',
+  'Git',
+  'AWS',
+
+  // Design
+  'UI/UX',
+  'Graphic Design',
+  'Figma',
+  'Adobe Photoshop',
+  'Video Editing',
+
+  // Business & Office
+  'Microsoft Excel',
+  'Microsoft Word',
+  'Project Management',
+  'Data Entry',
+  'Accounting',
+  'Bookkeeping',
+
+  // Marketing & Sales
+  'Digital Marketing',
+  'Social Media Management',
+  'Sales',
+  'Customer Service',
+  'SEO',
+
+  // Communication
+  'Public Speaking',
+  'Teamwork',
+  'Leadership',
+  'Communication',
+
+  // Education
+  'Teaching',
+  'Tutoring',
+
+  // Languages
+  'Arabic',
+  'English'
+]
+
+export default skills
