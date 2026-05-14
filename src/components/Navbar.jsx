63ba861fdb6096b879bea5d28a8b2f446d04de03
Saraalsobah <salsobah2006@gmail.com>
2026-05-14 17:40:32 +0300

    Fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,50 @@
-import { Link } from 'react-router'
+import { Link, useNavigate} from 'react-router'
 
 function Navbar({ user, setUser }) {
-
+  
 
   function logOut(){
     localStorage.removeItem('token')
     setUser(null)
+    navigate('/')
+    
   }
 
   return (
-    <div>
+    <nav>
       {/* Routes seen by everyone */}
-      <Link className='nav-item' to='/'>Homepage</Link>
+      <Link className='nav-item' to='/'>Wa6'fny</Link>
 
       {user ? (
-        // Links for protected routes only for logged in users
         <>
-        <Link className='nav-item' to='/dashboard'>Dashboard</Link>
+          {user.role === 'seeker' && (
+            <>
+              <Link className='nav-item' to='/jobs'>Browse Jobs</Link>
+              <Link className='nav-item' to='/applications'>My Applications</Link>
+            </>
+          )}
 
-        <span className='nav-item'>{user.username}</span>
-       
+          {user.role === 'employer' && (
+            <>
+              <Link className='nav-item' to='/dashboard'>Dashboard</Link>
+              <Link className='nav-item' to='/jobs/new'>Post a Job</Link>
+            </>
+          )}
+
+        <Link className='nav-item' to='/profile'>{user.username}</Link>
         <button className='nav-item' onClick={logOut}>Log Out</button>
-
 
         </>
       ) :
       (
-        // links for not logged in users
         <>
-        <Link className='nav-item' to='/sign-up'>Sign up</Link>
-        <Link className='nav-item' to='/sign-in'>Sign in</Link>
+        <Link className='nav-item' to='/sign-up'>Sign Up</Link>
+        <Link className='nav-item' to='/sign-in'>Sign In</Link>
 
         </>
       )
       }
-    </div>
+    </nav>
   )
 }
 
