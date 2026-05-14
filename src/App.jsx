99cbac951fc398500b378245857dd349d3ff83e7
Saraalsobah <salsobah2006@gmail.com>
2026-05-14 16:37:58 +0300

    added the routes in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import SignUp from './pages/Signup';
 import SignIn from './pages/SignIn';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
+import ProfileSetup from './pages/ProfileSetup';
+import Profile from './pages/Profile';
+import Jobs from './pages/Jobs';
+import JobDetail from './pages/JobDetails';
+import JobForm from './pages/JobForm';
+import Applications from './pages/Applications';
+import Applicants from './pages/Applicants';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -27,11 +34,18 @@ function App() {
     <div>
       <Navbar user={user} setUser={setUser} />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sign-up" element={!user ? <SignUp /> : <Navigate to='/dashboard'/>} />
-        <Route path="/sign-in" element={!user ? <SignIn setUser={setUser} /> : <Navigate to='/dashboard'/>} />
+        <Route path="/" element={<Homepage user={user} />} />
+        <Route path="/sign-up" element={!user ? <SignUp /> : <Navigate to='/'/>} />
+        <Route path="/sign-in" element={!user ? <SignIn setUser={setUser} /> : <Navigate to='/'/>} />
         <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to='/sign-in'/>} />
-
+        <Route path="/profile/setup" element={user ? <ProfileSetup user={user} /> : <Navigate to='/sign-in'/>} />
+        <Route path="/profile" element={user ? <Profile user={user} /> : <Navigate to='/sign-in'/>} />
+        <Route path="/jobs" element={user ? <Jobs /> : <Navigate to='/sign-in'/>} />
+        <Route path="/jobs/new" element={user ? <JobForm /> : <Navigate to='/sign-in'/>} />
+        <Route path="/jobs/:id" element={user ? <JobDetail user={user} /> : <Navigate to='/sign-in'/>} />
+        <Route path="/jobs/:id/edit" element={user ? <JobForm /> : <Navigate to='/sign-in'/>} />
+        <Route path="/jobs/:id/applicants" element={user ? <Applicants /> : <Navigate to='/sign-in'/>} />
+        <Route path="/applications" element={user ? <Applications /> : <Navigate to='/sign-in'/>} />
       </Routes>
     </div>
   );
